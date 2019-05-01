5604ccd0e511615110e226592156dc31e9b4e1c1
Holden B <holden.bucher.92@gmail.com>
2019-05-01 16:44:43 -0500

    changed tests to work for memoized solution

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -2,14 +2,24 @@
 
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+    curr_max = -10000
+    for i in range(0, len(prices)):
+        for j in range(i + 1, len(prices)):
+            if prices[j] - prices[i] > curr_max:
+                curr_max = prices[j] - prices[i]
+
+    return curr_max
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
